31c4e8f8bde1156524b461ef913283bb23230bc0
Tink <85289337+tink012184@users.noreply.github.com>
2025-10-30 20:35:04 -0500

    week 2 assignment and compressed pictures

--- data-files/rpg-character-builder/src/app/app.routes.ts
+++ data-files/rpg-character-builder/src/app/app.routes.ts
@@ -1,4 +1,16 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { PlayersComponent } from "./players/players.component";
+import { SigninComponent } from "./signin/signin.component";
+import { CreateCharacterComponent } from "./create-character/create-character.component";
+import { CreateGuildComponent } from "./create-guild/create-guild.component";
+import { CharacterFactionComponent } from "./character-faction/character-faction.component";
 
-export const routes: Routes = [{ path: "", component: HomeComponent }];
+export const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "players", component: PlayersComponent },
+  { path: "signin", component: SigninComponent },
+  { path: "create-character", component: CreateCharacterComponent },
+  { path: "create-guild", component: CreateGuildComponent },
+  { path: "character-faction", component: CharacterFactionComponent },
+];
